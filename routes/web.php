216baa86c9b1d1b754fe605d895bf8b29a26033e
Boris Bobol <bobol.boris@gmail.com>
2021-02-10 15:13:09 +0300

    07.08.2020 15:13

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,7 @@ $router->get('search', 'IndexController@searchAction');
 $router->get('update', 'IndexController@updateIndexAction');
 
 $router->get('api/update', 'UpdateController@updateAction');
-$router->get('api/categories/get[/{format}]', 'ApiController@categoriesSearchAction');
-$router->get('api/products/get[/{format}]', 'ApiController@productsSearchAction');
-$router->get('api/contact_categories/get[/{format}]', 'ApiController@contactCategoriesSearchAction');
+$router->get('api/products/get', 'ApiController@productsSearchAction');
+$router->get('api/categories/get', 'ApiController@categoriesSearchAction');
 $router->get('api/get_dollar_currency', 'ApiController@getDollarRateAction');
-
+$router->get('api/contact_categories/get', 'ApiController@contactCategoriesSearchAction');
